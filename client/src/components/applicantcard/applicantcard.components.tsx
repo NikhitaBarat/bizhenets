9a868bd5e07e271e7f67f8fabc3446a1896ff547
Siddhant Prateek <siddhantprateek@gmail.com>
2022-07-08 23:30:30 +0530

    applicant card component

--- client/src/components/applicantcard/applicantcard.components.tsx
+++ client/src/components/applicantcard/applicantcard.components.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import "./applicantcard.styles.css";
 
 const Applicantcard = () => {
   return (
-    <div>Applicantcard</div>
-  )
-}
+    <div className="application-card">
+      <div>
+        <div className="application-img-sec">
+          <img
+            className="application-img"
+            src={`https://pickaface.net/gallery/avatar/unr_randomavatar_170412_0236_9n4c2i.png`}
+            alt=""
+          />
+        </div>
+        <div className="form-section">
+          <form className="applicant-form" action="">
+            <div className="applying-to">Applying to: Harry Maguire</div>
+            <input className="full-cover applicant-inp" type="text" placeholder="Enter your full name" />
+            <input className="full-cover applicant-inp" type="text" placeholder="Location" />
+            <div className="horz-section">
+              <input className="half-cover applicant-inp" type="number" placeholder="Age" />
+              <input className="half-cover applicant-inp mrg-right" type="text" placeholder="Employment" />
+            </div>
+            <div className="horz-section">
+              <input className="half-cover applicant-inp" type="number" placeholder="Room for" />
+              <input className="half-cover applicant-inp mrg-right" type="text" placeholder="Criminal Status" />
+            </div>
+            <button type="submit" className="submit-application">SUBMIT APPLICATION</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Applicantcard;
